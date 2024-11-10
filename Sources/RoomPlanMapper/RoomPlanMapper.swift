4408876193780d719969053ee5e77fc5e08eecdb
Paolo Giua <p.giua@reply.it>
2024-11-10 18:32:36 +0100

    Code refactoring

--- Sources/RoomPlanMapper/RoomPlanMapper.swift
+++ Sources/RoomPlanMapper/RoomPlanMapper.swift
@@ -36,6 +36,7 @@ class RoomPlanMapper: NSObject {
         return worldMap
     }
     
+    @MainActor
     func startScanning() {
         // Set up the ARSCNView
         sceneView.delegate = self
